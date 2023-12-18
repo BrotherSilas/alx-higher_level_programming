fc2ac4b9e9af3c082f7ff9d7b190e4cde476a793
root <silasedetsnr@gmail.com>
2023-12-18 02:12:03 -0800

    if else loops

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+# -----------------------------------------------------------
+# Python function that computes the square value of all integers of a matrix
+#
+# (C) 2023, Silas Edet, Uyo, Nigeria
+# email: silasedetsnr@gmail.com
+# -----------------------------------------------------------
+
+for number in range(99):
+
+    # Print out the number and the hex value of number
+    print("{:d} = {}".format(number, hex(number)))
