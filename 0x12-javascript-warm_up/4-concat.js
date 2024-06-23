f8c9d89dad562e055aafbac7e9951a90c7f4720f
Silas Edet <silasedetsnr@gmail.com>
2024-06-23 09:29:52 -0700

    java

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const { argv } = require('process');
+
+const initialArg = argv[2];
+const nextArg = argv[3];
+
+if (initialArg === undefined) {
+  console.log('No argument');
+} else if (nextArg === undefined) {
+  console.log(initialArg + ' is undefined');
+} else {
+  console.log(initialArg + ' is ' + nextArg);
+}
