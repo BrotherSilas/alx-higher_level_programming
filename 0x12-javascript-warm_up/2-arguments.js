4f56eb9852b93649c6213f810d5ff319566ed7e0
Silas Edet <silasedetsnr@gmail.com>
2024-06-23 08:36:52 -0700

    java

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
-const {argv} = require("node:process");
+const { argv } = require('node:process');
 const length = argv.length - 2;
 
 if (length <= 0) {
-	console.log ("No argument");
-}
-else if (length === 1) {
-	console.log ("Argument found");
-}
-else {
-	console.log ("Arguments found");
+  console.log('No argument');
+} else if (length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
