a1a1f1cd0e2de6fecf997ba2421bc4233a3ff208
Silas Edet <silasedetsnr@gmail.com>
2024-07-01 05:00:21 -0700

    java

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -2,7 +2,7 @@
 
 let count = 0; // to keep track of the number of calls
 
-exports.logMe = function(item) {
-	console.log(`${count}: ${item}`);
-	count++; // count increment after previous printing
+exports.logMe = function (item) {
+  console.log(`${count}: ${item}`);
+  count++; // count increment after previous printing
 };
