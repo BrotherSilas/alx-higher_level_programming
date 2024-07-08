ac4dc1a6dd17301948fd97073c2dfb6628e166e2
Silas Edet <silasedetsnr@gmail.com>
2024-07-08 10:55:49 -0700

    java

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,17 +1,17 @@
 #!/usr/bin/node
 
 // import dict
-const {dict} = require('./101-data.js');
+const { dict } = require('./101-data.js');
 const newDict = {};
 
 // Iterate through the original dictionary
 for (const [userId, occurrences] of Object.entries(dict)) {
-    // If the occurrence number doesn't exist as a key, create an empty array
-    if (!newDict[occurrences]) {
-        newDict[occurrences] = [];
-    }
-    // Add the user id to the array for this occurrence number
-    newDict[occurrences].push(userId);
+  // If the occurrence number doesn't exist as a key, create an empty array
+  if (!newDict[occurrences]) {
+    newDict[occurrences] = [];
+  }
+  // Add the user id to the array for this occurrence number
+  newDict[occurrences].push(userId);
 }
 
 // Print the new dictionary
